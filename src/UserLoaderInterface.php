c64f612dd041ae5611249241b2aef687f1ae2945
github-ci <you@example.com>
2025-05-10 12:17:44 +0000

    feat: 添加 UserLoaderInterface 中 createUser 方法

--- src/UserLoaderInterface.php
+++ src/UserLoaderInterface.php
@@ -2,9 +2,13 @@
 
 namespace Tourze\WechatMiniProgramUserContracts;
 
+use Tourze\WechatMiniProgramAppIDContracts\MiniProgramInterface;
+
 interface UserLoaderInterface
 {
     public function loadUserByOpenId(string $openId): ?UserInterface;
 
     public function loadUserByUnionId(string $unionId): ?UserInterface;
+
+    public function createUser(MiniProgramInterface $miniProgram, string $openId, ?string $unionId = null): UserInterface;
 }
